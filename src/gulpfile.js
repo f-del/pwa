7a7e1758513e91dbfc732a6906b7baa55d098808
fabien delahousse <fabien@fabinux>
2017-06-26 23:55:01 +0200

    Add Gulp + js minification build

---
+++ src/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),    
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    // uglify = require('gulp-uglify'), can't on ly uglify ES5 js code (see https://github.com/terinjokes/gulp-uglify#using-a-different-uglifyjs for es6 support)
+    notify = require('gulp-notify');
+
+gulp.task('scripts', function() {
+  return gulp.src('./public/scripts/**/*.js')
+    //.pipe(jshint('.jshintrc'))
+    //.pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('../dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    //.pipe(uglify()) 
+    .pipe(gulp.dest('../dist/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+
+gulp.task('default', [/*'clean'*/], function() {
+    //gulp.start('styles', 'scripts', 'images');
+    gulp.start('scripts');
+});
